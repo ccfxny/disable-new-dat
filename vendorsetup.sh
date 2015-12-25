bfb25a81dfa86064c654fbfd569963ad1d005736
ccfxny <9886712@126.com>
2015-12-25 18:53:36 +0800

    add openjdk path before me use default sun jdk

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,4 +9,9 @@ then
   echo ""
   echo "***************** Disable make .new.data file *****************"
   echo ""
+echo "***************** Set open jdk *****************"
+export JAVA_HOME=/usr/lib/jvm
+export JRE_HOME=/usr/lib/jvm/jre
+export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH:$JAVA_HOME/bin
+export CLASSPATH=./:$JAVA_HOME/lib:$JAVA_HOME/jre/lib
 fi
