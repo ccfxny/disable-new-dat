97164417f5e81548d4c3aec375344b7c1f7350b0
ccfxny <9886712@126.com>
2016-07-01 16:18:56 +0800

    update JAVA_HOME=/usr/lib/jvm/java
    fix java home

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,7 +10,7 @@ then
   echo "***************** Disable make .new.data file *****************"
   echo ""
 echo "***************** Set open jdk *****************"
-export JAVA_HOME=/usr/lib/jvm
+export JAVA_HOME=/usr/lib/jvm/java
 export JRE_HOME=/usr/lib/jvm/jre
 export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH:$JAVA_HOME/bin
 export CLASSPATH=./:$JAVA_HOME/lib:$JAVA_HOME/jre/lib
